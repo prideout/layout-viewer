6b7e9459272ab820588a1028ef9b651ce97f80bf
Philip Rideout <philiprideout@gmail.com>
2025-03-14 22:49:12 -0700

    Add a gitignore.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,14 +4,14 @@ use wasm_bindgen::prelude::*;
 
 #[derive(Debug)]
 pub struct LayoutStats {
-    pub cell_count: usize,
-    pub total_polygons: usize,
-    pub total_paths: usize,
-    pub total_srefs: usize,
-    pub total_arefs: usize,
-    pub total_texts: usize,
-    pub total_nodes: usize,
-    pub total_boxes: usize,
+    pub struct_count: usize,
+    pub polygon_count: usize,
+    pub path_count: usize,
+    pub sref_count: usize,
+    pub aref_count: usize,
+    pub text_count: usize,
+    pub node_count: usize,
+    pub box_count: usize,
 }
 
 #[wasm_bindgen]
@@ -28,27 +28,27 @@ impl Layout {
             .map_err(|e| JsValue::from_str(&format!("Failed to parse GDSII: {}", e)))?;
 
         let mut stats = LayoutStats {
-            cell_count: library.structs.len(),
-            total_polygons: 0,
-            total_paths: 0,
-            total_srefs: 0,
-            total_arefs: 0,
-            total_texts: 0,
-            total_nodes: 0,
-            total_boxes: 0,
+            struct_count: library.structs.len(),
+            polygon_count: 0,
+            path_count: 0,
+            sref_count: 0,
+            aref_count: 0,
+            text_count: 0,
+            node_count: 0,
+            box_count: 0,
         };
 
         // Count elements in each cell
         for gds_struct in &library.structs {
             for element in &gds_struct.elems {
                 match element {
-                    gds21::GdsElement::GdsBoundary(_) => stats.total_polygons += 1,
-                    gds21::GdsElement::GdsPath(_) => stats.total_paths += 1,
-                    gds21::GdsElement::GdsStructRef(_) => stats.total_srefs += 1,
-                    gds21::GdsElement::GdsArrayRef(_) => stats.total_arefs += 1,
-                    gds21::GdsElement::GdsTextElem(_) => stats.total_texts += 1,
-                    gds21::GdsElement::GdsNode(_) => stats.total_nodes += 1,
-                    gds21::GdsElement::GdsBox(_) => stats.total_boxes += 1,
+                    gds21::GdsElement::GdsBoundary(_) => stats.polygon_count += 1,
+                    gds21::GdsElement::GdsPath(_) => stats.path_count += 1,
+                    gds21::GdsElement::GdsStructRef(_) => stats.sref_count += 1,
+                    gds21::GdsElement::GdsArrayRef(_) => stats.aref_count += 1,
+                    gds21::GdsElement::GdsTextElem(_) => stats.text_count += 1,
+                    gds21::GdsElement::GdsNode(_) => stats.node_count += 1,
+                    gds21::GdsElement::GdsBox(_) => stats.box_count += 1,
                 }
             }
         }
@@ -75,6 +75,9 @@ impl Layout {
     }
     pub fn name(&self) -> &str {
         &self.library.name
+    }
+    pub fn library(&self) -> &GdsLibrary {
+        &self.library
     }
     pub fn process_gds_file(data: &[u8]) -> Result<Self> {
         Self::from_bytes(data).map_err(|e| {
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,8 +1,12 @@
 use anyhow::{anyhow, Result};
 use colored::*;
 use layout_viewer::Layout;
+use std::collections::HashMap;
 use std::fs;
 use std::path::Path;
+
+#[derive(Debug, Clone, Copy, Hash, Eq, PartialEq)]
+struct CellId(usize);
 
 struct StatsRow {
     name: ColoredString,
@@ -15,6 +19,36 @@ impl StatsRow {
             name: name.to_string().color(Color::Green),
             value,
         }
+    }
+}
+
+struct StringInterner {
+    strings: Vec<String>,
+    indices: HashMap<String, CellId>,
+}
+
+impl StringInterner {
+    fn new() -> Self {
+        Self {
+            strings: Vec::new(),
+            indices: HashMap::new(),
+        }
+    }
+
+    fn intern(&mut self, s: String) -> CellId {
+        if let Some(&idx) = self.indices.get(&s) {
+            idx
+        } else {
+            let idx = CellId(self.strings.len());
+            self.indices.insert(s.clone(), idx);
+            self.strings.push(s);
+            idx
+        }
+    }
+
+    #[allow(dead_code)]
+    fn get(&self, id: CellId) -> &str {
+        &self.strings[id.0]
     }
 }
 
@@ -70,18 +104,80 @@ fn main() -> Result<()> {
 
     let stats = layout.stats();
     let stats_rows = vec![
-        StatsRow::new("Cells", stats.cell_count),
-        StatsRow::new("Polygons", stats.total_polygons),
-        StatsRow::new("Paths", stats.total_paths),
-        StatsRow::new("SRefs", stats.total_srefs),
-        StatsRow::new("ARefs", stats.total_arefs), // not present in our test files
-        StatsRow::new("Texts", stats.total_texts), // present in caravel but let's ignore
-        StatsRow::new("Nodes", stats.total_nodes), // not present in our test files
-        StatsRow::new("Boxes", stats.total_boxes), // not present in our test files
+        StatsRow::new("Structs", stats.struct_count),
+        StatsRow::new("Polygons", stats.polygon_count),
+        StatsRow::new("Paths", stats.path_count),
+        StatsRow::new("SRefs", stats.sref_count),
+        StatsRow::new("ARefs", stats.aref_count), // not present in our test files
+        StatsRow::new("Texts", stats.text_count), // present in caravel but let's ignore
+        StatsRow::new("Nodes", stats.node_count), // not present in our test files
+        StatsRow::new("Boxes", stats.box_count),  // not present in our test files
     ];
 
     for row in stats_rows {
         println!("{:<12} {}", row.name, row.value);
+    }
+
+    let mut interner = StringInterner::new();
+
+    // Maps from a cell name to the cells that reference it
+    let mut hierarchy: HashMap<CellId, Vec<CellId>> = HashMap::new();
+
+    for cell in &layout.library().structs {
+        let cell_idx = interner.intern(cell.name.clone());
+        for elem in &cell.elems {
+            if let gds21::GdsElement::GdsStructRef(sref) = elem {
+                let ref_idx = interner.intern(sref.name.clone());
+                hierarchy.entry(ref_idx).or_default().push(cell_idx);
+            }
+            if let gds21::GdsElement::GdsArrayRef(aref) = elem {
+                let ref_idx = interner.intern(aref.name.clone());
+                hierarchy.entry(ref_idx).or_default().push(cell_idx);
+            }
+        }
+    }
+
+    for cell in &layout.library().structs {
+        let cell_idx = interner.intern(cell.name.clone());
+        let is_root = !hierarchy.contains_key(&cell_idx);
+        let poly_count = cell
+            .elems
+            .iter()
+            .filter(|e| matches!(e, gds21::GdsElement::GdsBoundary(_)))
+            .count();
+        let path_count = cell
+            .elems
+            .iter()
+            .filter(|e| matches!(e, gds21::GdsElement::GdsPath(_)))
+            .count();
+        let shape_count = poly_count + path_count;
+        let sref_count = cell
+            .elems
+            .iter()
+            .filter(|e| matches!(e, gds21::GdsElement::GdsStructRef(_)))
+            .count();
+        let aref_count = cell
+            .elems
+            .iter()
+            .filter(|e| matches!(e, gds21::GdsElement::GdsArrayRef(_)))
+            .count();
+        let children_count = sref_count + aref_count;
+        let mut output = format!(
+            "{} :: {} shapes",
+            if is_root {
+                cell.name.color(Color::BrightYellow)
+            } else {
+                cell.name.color(Color::White)
+            },
+            shape_count.to_string().color(Color::BrightWhite),
+        );
+        if children_count > 0 {
+            output.push_str(&format!(", {} children", children_count));
+        }
+        if !is_root {
+            output.push_str(&format!(", {} parents", hierarchy[&cell_idx].len()));
+        }
+        println!("{}", output);
     }
 
     // Generate and save SVG only if output path is provided
